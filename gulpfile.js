a68b4e1b7175591f3d580252fa047336b69b9ead
Dave Lochhead <dave.lochhead@dogdigital.com>
2015-07-21 10:09:03 +0100

    Added run-sequence so we can build the CSS before starting dev watches.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,31 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+var runSequence = require('run-sequence');
 
 var path = {
     source: 'www_src',
     build: 'www_dist'
 };
 
-gulp.task('styles_dev', function()
-{
-    var stream = gulp.src(path.source + '/scss/main.scss')
-        .pipe(sass(
-        {
+gulp.task('dev:styles', function() {
+    var stream = gulp.src(path.source + '/_scss/main.scss')
+        .pipe(sass({
             style: 'expanded'
         }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('www_src/css'));
+        .pipe(gulp.dest(path.source + '/css'));
 
     return stream;
 });
 
 
-gulp.task('dev', function()
-{
+gulp.task('dev', function() {
+    
     var server = livereload.listen();
 
-    gulp.watch(path.source + '/scss/**/*.scss', ['styles_dev']);
+    gulp.watch(path.source + '/_scss/**/*.scss', ['dev:styles']);
 
     gulp.watch(path.source + '/css/*.css').on('change', livereload.changed);
 
@@ -38,4 +37,6 @@ gulp.task('dev', function()
 });
 
 // Default task
-gulp.task('default', ['dev']);
+gulp.task('default', function() {
+    runSequence('dev:styles', ['dev']);
+});
